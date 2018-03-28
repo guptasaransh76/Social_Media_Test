447306d88724f7ea49f8970f94d37198e94f7af8
Saransh Gupta <sj@Saranshs-MacBook-Pro.local>
2018-03-28 20:06:43 +0530

    initial routes configured #5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  #define root address
+  root 'pages#index'
+
+  #define routes for pages
+  get 'index' => 'pages#index'
+  get 'home' => 'pages#home'
+  get 'profile' => 'pages#profile'
+  get 'explore' => 'pages#explore'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
